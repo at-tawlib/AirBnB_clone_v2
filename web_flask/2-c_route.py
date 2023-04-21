54b68d94d9c6852019ad881e428da5d55889db1a
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-04-21 17:25:51 +0000

    Create 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Starts Flask web app listening on 0.0.0.0, port 5000"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """route to print out a text"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """Displays "C" followed by the value of text"""
+    return f"C {text}"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
