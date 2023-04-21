7c192e93c3dae868b4e8af7fd2dfd97e6a39abf6
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-04-21 18:50:03 +0000

    Create 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Starts Flask web app listening on 0.0.0.0, port 5000"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """route to print out a text"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """Displays "C" followed by the value of text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python_text(text="is cool"):
+    """
+    Displays 'Python followed by the value of text variable
+    Default value of text is 'is cool'
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
