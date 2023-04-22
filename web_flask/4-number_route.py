1aaa93125c1440516dcd59c5ca3f2203eade255c
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-04-22 21:07:45 +0000

    Create 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Starts Flask web app listening on 0.0.0.0, port 5000"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """route to print out a text"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """Displays "C" followed by the value of text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python_text(text="is cool"):
+    """
+    Displays 'Python followed by the value of text variable
+    Default value of text is 'is cool'
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>")
+def display_n(n):
+    """Displays 'n is a number' only if n is an integer"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
