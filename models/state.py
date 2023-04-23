d0566192bf7fbf5b2daffcf9bec37e158bb468d7
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-04-23 13:31:26 +0000

    Update cities relationship

--- models/state.py
+++ models/state.py
@@ -10,12 +10,12 @@ import models
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref='state',
-                              cascade="all, delete, delete-orphan")
+        cities = relationship("City", backref='states',
+                              cascade="all, delete")
     else:
         name = ''
 
