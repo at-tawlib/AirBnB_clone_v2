2c10e5df33de27c47bbf33cddabc23e94fdf9bbb
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-04-23 20:43:32 +0000

    clean upd

--- models/state.py
+++ models/state.py
@@ -27,12 +27,12 @@ class State(BaseModel, Base):
         @property
         def cities(self):
             """
-            fs getter attribute that returns City instances
+            filestorage getter attribute that returns City instances
             state_id equals the current state
             """
-            values_city = models.storage.all("City").values()
+            cities_values = models.storage.all("City").values()
             list_city = []
-            for city in values_city:
+            for city in cities_values:
                 if city.state_id == self.id:
                     list_city.append(city)
             return list_city
