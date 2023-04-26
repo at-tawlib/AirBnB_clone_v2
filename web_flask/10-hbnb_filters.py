2fb192de9224dbe4efc0192fc9f46ce855103fc8
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-04-26 07:28:34 +0000

    Create 10-hbnb_filters.py

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -2,6 +2,8 @@
 """Starts Flask web app listening on 0.0.0.0, port 5000"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+from models.amenity import Amenity
 
 
 app = Flask(__name__)
@@ -9,11 +11,11 @@ app.url_map.strict_slashes = False
 
 
 @app.route('/hbnb_filters')
-def hbnb_filters():
-    """display a beautiful HTML with loaded data"""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
-    return render_template('6-index.html', amenities=amenities, states=states)
+def filters():
+    """ display filtered states in HTML page"""
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template('6-index.html', states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
